de49696c5a19234e094e966cf07df4643db462c2
6zev <h0dev@hotmail.com>
2026-06-23 07:43:25 +0000

    refactor: remove in-tree core framework, depend on core-parsers
    - Delete core/, model/, network/, util/, config/, exception/, bitmap/
    - Remove root framework files (MangaParser, MangaLoaderContext, etc.)
    - Keep all 58 site-specific parsers and base parsers
    - Add core.parsers dependency to build.gradle.kts and libs.versions.toml
    - Framework now provided by com.github.UsagiApp:core-parsers

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.collection)
     implementation(libs.nanohttpd)
     implementation(libs.gson)
+    implementation(libs.core.parsers)
 
     ksp(project(":plugins-ksp"))
 
